9a0d06fd02de0088ac91972e8f9b3a55dcbe2ce8
LayaDelta <juanlaya0406@gmail.com>
2024-03-02 19:22:27 -0400

    Actualizacion de nombres de las constantes y mejorando el boton de busqueda

--- src/componentes/Molecules/ButtonsIsland/BotonIslands.jsx
+++ src/componentes/Molecules/ButtonsIsland/BotonIslands.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import ButtonIsland from "../../atom/ButtonIslands/ButtonIsland";
+import Styles from "./BotonIsland.module.css"
 
 
 
 export function BotonIsland() {
     return(
         <Fragment>
-            <section>
+            <section className={Styles.Fc_Section}>
                 <ButtonIsland Icon="house.svg"/>
                 <ButtonIsland Icon="AddFriends.svg"/>
                 <ButtonIsland Icon="Friends.svg"/>
--- src/componentes/atom/Screach/Screach.jsx
+++ src/componentes/atom/Screach/Screach.jsx
@@ -1,14 +1,48 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useRef } from "react";
 import Styles from "./Screach.module.css"
+import { useState } from "react";
+
 export function Screach() {
+    const [clase, setClase] = useState( Styles.Fc_Nav);
+    const ref = useRef(null);
+    const [claseBotton, setClaseBotton] = useState("Screach.svg");
+    const refButton = useRef(null)
+
+    const handleClick = () => {
+        if (clase === Styles.Fc_Nav) {
+            setClase(Styles.Fc_Nav_Clik);
+            setClaseBotton("Arrow.svg")
+          } else {
+            setClase(Styles.Fc_Input);
+            setClaseBotton("Screach.svg")
+          }
+    };
+
+
+    const handleOutsideClick = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setClase(Styles.Fc_Nav);
+          setClaseBotton("Screach.svg")
+          
+        }
+      };
+    
+      useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+    
+        return () => {
+          document.removeEventListener('mousedown', handleOutsideClick);
+        };
+      }, []);
+      const ImgButton = `../src/assets/Buttons/${claseBotton}`
 
 return(
     <Fragment>
-        <nav className={Styles.Fc_Nav}>
+        <nav className={clase} ref={ref}>
         <button className={Styles.Fc_Button}>
-            <img className={Styles.Fc_Button_Img} src="./src/assets/Buttons/Screach.svg" alt="" />
+            <img className={Styles.Fc_Button_Img} src={ImgButton} alt="" />
         </button>
-        <input className={Styles.Fc_Input} type="text" placeholder="Buscar en Facebook..." />
+        <input className={Styles.Fc_Input} onClick={handleClick} type="text" placeholder="Buscar en Facebook..." />
         </nav>
     </Fragment>
 
